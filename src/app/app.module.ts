73e1cf7791f4db42fa5248875f0e2078e6bac6c1
XtreMeIGravity <xtremeigraviti@hotmail.com>
2019-12-01 00:21:02 -0600

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { MiComponente } from './components/mi-componente/mi-componente.component';
 import { PruebaComponent } from './components/prueba/prueba.component';
 import { FelizComponent } from './components/feliz/feliz.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MiComponente,
     PruebaComponent,
-    FelizComponent
+    FelizComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
