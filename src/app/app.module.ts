ad0e88128cdc274534cb6dc8398a65582ceec55f
XtreMeiGravity <xtremeigraviti@hotmail.com>
2019-12-03 14:47:24 -0600

    git service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { Componente1Component } from './componentes/componente1/componente1.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MaterialModule} from './material.module';
+
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, NgbModule],
+  declarations: [AppComponent, Componente1Component],
+  imports: [
+    BrowserModule, 
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
